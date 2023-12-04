cbc10471ce4e6354d17b8692aa93891ee6882a91
Dan <daniel.speranskiy@live.com>
2023-12-04 22:05:45 +0400

    Added a search bar UI

---
+++ client/src/components/Searchbar.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { userProps } from '@/types'
+import React from 'react'
+import Image from 'next/image'
+import { SearchIcon } from '@/utils/icons'
+
+const Searchbar = ({user}: {user: userProps}) => {
+  return (
+    <div className='flex gap-4 '>
+        <div className='avatar online py-1'>
+            <div className='w-10 rounded-full ring '>
+                <Image src={user?.imageId || 'https://robohash.org/user.png'} alt='avatar' width={256} height={256} className='rounded-full' />
+            </div>
+        </div>
+        
+        <div className='relative w-full '>
+            <input type='text' placeholder='Search' className='input input-bordered w-full pl-8 rounded-full bg-gray-100 placeholder:text-gray-600' />
+            <div className='w-6 h-6 absolute top-1/2 left-5 -translate-x-1/2 -translate-y-1/2'>
+            <SearchIcon  />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Searchbar
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -5,20 +5,26 @@ import { useUser } from '@/store/userStore'
 import React, { useEffect, useState } from 'react'
 import {useCookies} from 'react-cookie'
 import { shallow } from 'zustand/shallow'
+import Searchbar from './Searchbar'
 
 const Sidebar = () => {
 const [cookie, setCookie] = useCookies(["user"])
+
 const {myUser, setUser} = useUser((state) => ({
     myUser: state.myUser,
     setUser: state.setUser
 }), shallow)
+
+
     useEffect(() => {
        fetchUser(cookie,setUser)
     },[cookie.user])
+
+
   return (
     <div className='w-full md:block sidebar z-10 border border-r-2 border-slate-300 md:w-1/2 lg:w-1/3 p-3 bg-white h-screen '>
         {/* searchbae compt */}
-        
+        <Searchbar user={myUser}  />
 
         {/* chat list */}
 
